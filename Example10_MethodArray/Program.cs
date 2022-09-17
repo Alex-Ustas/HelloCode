063ce8326d4eae6a0c2e08f381892b4bb2d445f1
Alex Ustas <alex.ustas@internet.ru>
2022-09-17 13:54:35 +0300

    add examples 8-11 - functions and arrays

---
+++ Example10_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 13, 25, 73, 94, 15, 86, 47, 38, 29 };
+
+int n = array.Length;
+int find = 38;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
