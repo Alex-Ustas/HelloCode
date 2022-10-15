f9eaa7c3e42756ae827f4e3c5ad568cf1ab9f1f0
Alex Ustas <alex.ustas@internet.ru>
2022-10-15 16:24:03 +0300

    add second part to example 14

--- Example014_GoodCode/Program.cs
+++ Example014_GoodCode/Program.cs
@@ -6,9 +6,30 @@ Console.Clear();
 Console.WriteLine(a.GetType());
 Console.WriteLine(a.GetType().Name);
 
-var data = new int[] { 1, 2, 3, 4 }
-    .Where(e => e > 0)
-    .Select(e => new { q = e, w = e + 1 });
-Console.WriteLine(data.GetType());
-Console.WriteLine(data.GetType().Name);
+// var data = new int[] { 1, 2, 3, 4 }
+//     .Where(e => e > 0)
+//     .Select(e => new { q = e, w = e + 1 });
+// Console.WriteLine(data.GetType());
+// Console.WriteLine(data.GetType().Name);
 
+string text = "(1,2) (2,3) (4,5) (9,7)"
+    .Replace("(", "")
+    .Replace(")", "");
+
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+    .Select(item => item.Split(','))
+    .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+    .Where(e => e.x % 2 == 0)
+    .Select(point => (point.x * 10, point.y + 10))
+    .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
+
+///
+/// Вычисление координаты
+///
